7797d5eaae6b1d5cce7c4f4d624616c1d0e23a98
Felix2114 <zS23004696@estudiantes.uv.mx>
2025-06-19 02:48:47 -0600

    update

--- piglets_api/settings.py
+++ piglets_api/settings.py
@@ -132,6 +132,7 @@ if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 GRAPHENE = {
     'SCHEMA': 'piglets_api.schema.schema',
